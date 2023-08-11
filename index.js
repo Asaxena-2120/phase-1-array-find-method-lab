9b5db10c7378f1add5f7c4c09bdcfcbbb8ee1932
Aastha <saxena.aastha91@gmail.com>
2023-08-11 14:11:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record){
+    const result = record.find((item)=>{
+        if(item.result==="W"){
+            return item
+        }
+ 
+        
+    })
+    return result ? result.year : undefined
+
+}
